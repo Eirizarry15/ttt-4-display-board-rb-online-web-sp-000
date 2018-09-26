c5d4de3932c60cdf88d2bc373d55bd99e9f92d28
Edgar Irizarry <eirizarry15@yahoo.com>
2018-09-26 19:04:19 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,2 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["O"," "," "," ","X"," "," "," "," "]
-
-def display_board(board)
-       |   |   
-    -----------
-       |   |   
-    -----------
-       |   |   
-end
